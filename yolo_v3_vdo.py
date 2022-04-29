8197f7523f4de08f131d37610a4298d10bbc76f9
Vedant-K1 <72189680+Vedant-K1@users.noreply.github.com>
2022-04-30 00:43:16 +0530

    Add files via upload

---
+++ yolo_v3_vdo.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import argparse
+import time
+
+
+def start_video(video_path):
+	model, classes, colors, output_layers = load_yolo()
+	cap = cv2.VideoCapture(video_path)
+	while True:
+		_, frame = cap.read()
+		height, width, channels = frame.shape
+		blob, outputs = detect_objects(frame, model, output_layers)
+		boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+		draw_labels(boxes, confs, colors, class_ids, classes, frame)
+		key = cv2.waitKey(1)
+		if key == 27:
+			break
+	cap.release()
+
+def start_video(video_path):
+    model, classes, colors, output_layers = load_yolo()
+    cap = cv2.VideoCapture(video_path)
+    while True:
+        _, frame = cap.read()
+        height, width, channels = frame.shape
+        blob, outputs = detect_objects(frame, model, output_layers)
+        boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+        draw_labels(boxes, confs, colors, class_ids, classes, frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    cap.release()
+
+
+def detect_objects(img, net, outputLayers):
+    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(outputLayers)
+    return blob, outputs
